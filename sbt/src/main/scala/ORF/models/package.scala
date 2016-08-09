846818bb6cc2769652313adaf2b3ec967d2c0353
Arthur Lui <luiarthur@gmail.com>
2016-08-09 14:25:26 -0700

    fixed bug in normalize

--- sbt/src/main/scala/ORF/models/package.scala
+++ sbt/src/main/scala/ORF/models/package.scala
@@ -18,7 +18,7 @@ package object models {
     val k = X(0).size
     Vector.range(0,n) map { i =>
       Vector.range(0,k) map { j => 
-        ( X(i)(j) - rng(j)._1 ) / ( rng(j)._2 - rng(j)._1)
+        ( X(i)(j) - rng(j)._2 ) / ( rng(j)._1 - rng(j)._2)
       }
     }
   }
