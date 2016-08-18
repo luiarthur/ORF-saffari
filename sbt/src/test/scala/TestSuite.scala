0c5ce21f019ab6a516b0a1ff9fdbc81b898b2617
Arthur Lui <luiarthur@gmail.com>
2016-08-18 09:07:14 -0700

    Removed Timer.scala and added function to models/package.scala

--- sbt/src/test/scala/TestSuite.scala
+++ sbt/src/test/scala/TestSuite.scala
@@ -2,7 +2,6 @@ import org.scalatest.FunSuite
 
 class TestSuite extends FunSuite {
   def round(x: Double, d: Int = 2) = (scala.math.pow(10,d) * x).toInt / scala.math.pow(10,d)
-  import Timer.time
 
   test("Drawing / testing trees") {
     import ORF.Tree
--- sbt/src/test/scala/Timer.scala
+++
@@ -1,9 +0,0 @@
-object Timer{
-  def time[R](block: => R): R = {  
-      val t0 = System.nanoTime()
-      val result = block    // call-by-name
-      val t1 = System.nanoTime()
-      println("\nElapsed time: " + (t1 - t0) / 1E9 + "s\n")
-      result
-  }
-}
